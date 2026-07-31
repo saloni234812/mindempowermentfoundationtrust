1bbc42faec66f654ea9b7dbb329baa55e71af5c3
saloni234812 <salonisingh09839@gmail.com>
2026-07-31 17:19:13 +0530

    Remove GhostCursor component globally from root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import { AccessibilityProvider } from "@/components/AccessibilityContext";
 import WhatsAppChat from "@/components/WhatsAppChat";
-import GhostCursor from "@/components/GhostCursor";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -37,14 +36,6 @@ export default function RootLayout({
         <AccessibilityProvider>
           {children}
           <WhatsAppChat />
-          <GhostCursor 
-            color="#E04F00"
-            brightness={1.2}
-            edgeIntensity={0}
-            trailLength={55}
-            inertia={0.4}
-            zIndex={9999}
-          />
         </AccessibilityProvider>
       </body>
     </html>
